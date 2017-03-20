02b821cb02f748f1a5d373856943a2538a3241c7
Anoop Thirthahalli <Anoop Thirthahalli>
2017-03-20 23:52:26 +0530

    Name Counter

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+(function () {
+  'use strict';
+  var app = angular.module("NameCalculator",[]);
+    app.controller("NameCalculatorController", function($scope) {
+     $scope.name = "";
+    $scope.totalValue=0;
+    $scope.displayNumeric=function () {
+     var totalNmaeValue=calculateNumericString($scope.name);
+      $scope.totalValue=totalNmaeValue;
+    };
+    
+    function calculateNumericString(string) {
+      var totString=0;
+      for(var i=0;i<string.length;i++)
+      {
+        totString+=string.charCodeAt(i);
+      }
+      return totString;
+      }
+  });
+})();
